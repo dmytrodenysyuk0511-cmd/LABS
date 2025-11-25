ae55cc91b10b68a168c8dc813c20c5c3bbecbe68
dmytrodenysyuk0511-cmd <dmytro.denysyuk.05.11@gmail.com>
2025-11-25 17:54:16 +0200

    Add files via upload

---
+++ LAB1.py
@@ -0,0 +1,30 @@
+age = 19
+weight = 60.2
+list_fruits = ['apple', 'orange','lemon']
+list_cars = ['amr','mercedes','audi']
+tuple_digits = (1,2,3,4,5,6,7,8,9,0)
+it_is_python = True
+unique_numbers = {14,87,94,881}
+height = 174
+students_in_group = 34
+year = 2025
+print(age)
+print(weight)
+print(list_fruits)
+print(list_cars)
+print(tuple_digits)
+print(it_is_python)
+print(unique_numbers)
+print(height)
+print(students_in_group)
+print(year)
+print(type(age))
+print(type(weight))
+print(type(list_fruits))
+print(type(list_cars))
+print(type(tuple_digits))
+print(type(it_is_python))
+print(type(unique_numbers))
+print(type(height))
+print(type(students_in_group))
+print(type(year))
