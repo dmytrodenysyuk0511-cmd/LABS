2a1dcc9480b1009c7982c6f6a1a266244ad31e73
dmytrodenysyuk0511-cmd <dmytro.denysyuk.05.11@gmail.com>
2025-12-10 13:05:19 +0200

    Refactor LAB2.py for list processing and sorting

--- LAB2.py
+++ LAB2.py
@@ -1,12 +1,18 @@
-main_list = [8, 21, 6, 843, 5, 34, 89, 777, 45, 2,
-             "apple", "pear", "banana", "kiwi", "cherry",
-             "papaya", "mandarine", "pineapple", "watermelon", "melon"]
-integer_list = sorted([x for x in main_list if isinstance(x, int)])
-string_list = sorted([x for x in main_list if isinstance(x, str)])
-sorted_list = integer_list + string_list
-paired_list = [x for x in integer_list if x % 2 == 0]
-caps_list = [x.upper() for x in string_list]
-print("Головний список:", main_list)
-print("Відсортований список:", sorted_list)
-print("Парні числа:", paired_list)
-print("CAPS:", caps_list)
+ist=[1, 8, 5, 9, 188, 1532, 67, 52,'a','n','h','d','e','b','g','i','l','v']
+list_str=[x for x in list if isinstance(x, str)] #isinstance це функція яяка перевіряє чи обєкт є екземпляром певного класу
+list_int=[x for x in list if isinstance(x, int)]
+list_int.sort() # сортує список
+list_str.sort() # сортує список
+list_sort=list_int.copy() # повертає копію списку
+list_sort.extend(list_str) # об'єднує списки
+list_2=[]
+for n in list_int:
+    if n%2==0:
+        list_2.append(n) # добавляє елемент в кінець списку
+list_STR=[]
+for x in list_str:
+    list_STR.append(x.upper())
+print('Початковий список:',list) # виводить початковий список
+print('Сортований список:',list_sort) # виводить відсортований список
+print('Список кратних 2:',list_2) # виводить список кратних 2
+print('Список капсом:',list_STR) # виводить список капсом
